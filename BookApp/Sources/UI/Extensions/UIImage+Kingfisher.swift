5c8d3675633953cfcda549173f6c3ba52e1ec105
username <email@blahblah.com>
2023-02-07 14:05:04 +0200

    Kingfisher cache increased

--- BookApp/Sources/UI/Extensions/UIImage+Kingfisher.swift
+++ BookApp/Sources/UI/Extensions/UIImage+Kingfisher.swift
@@ -10,6 +10,10 @@ import Kingfisher
 
 extension UIImageView {
     public func setImage(_ string: String) {
+        let cache = ImageCache.default
+        cache.memoryStorage.config.totalCostLimit = 1024 * 1024 * 10
+        cache.diskStorage.config.sizeLimit = 1024 * 1024 * 100
+        
         let url = URL(string: string)
         kf.indicatorType = .activity
         kf.setImage(with: url)
